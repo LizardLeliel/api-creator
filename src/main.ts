0fa365db4a2be3c899346ee67ce6fd67d037cf75
LizardLeliel <shadow.shade.shine@gmail.com>
2022-11-08 10:42:40 -0400

    Added bootstrap-vue-3

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,16 @@ import router from "./router";
 
 import "./assets/main.css";
 
+import { BootstrapVue3 } from 'bootstrap-vue-3';
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+
+app.use(BootstrapVue3);
 
 app.mount("#app");
