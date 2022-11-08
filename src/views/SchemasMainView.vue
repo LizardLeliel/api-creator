c16b31bab460eb186696d2dfaf06ab08bd5e37fe
LizardLeliel <shadow.shade.shine@gmail.com>
2022-11-08 14:49:01 -0400

    Added ability to add new schemas and new fields to most recent schema

--- src/views/SchemasMainView.vue
+++ src/views/SchemasMainView.vue
@@ -2,41 +2,52 @@
     <h1> Schemas </h1>
 
     <div v-for="schema in schemas">
+        <h2> Schema: {{ schema.name }} </h2>
         {{ JSON.stringify(schema.getFields()) }}
         <hr>
     </div>
 
+    Schema Name: <input v-model="newSchemaName" type="text" /> <br>
     <button class="btn btn-dark" @click="addNewSchema">
-        New Schema?!?
-    </button>
+        Create New Schema named "{{newSchemaName}}"
+    </button> <br>
+    
+    <br>
 
-    <button class="btn btn-dark" @click="updateLastSchema">
-        Schema gets new field?!?
-    </button>
+    <template v-if="schemaSet == true">
+        Field Name: <input v-model="newFieldName" type="text" /> <br>
+        <button class="btn btn-dark" @click="updateLastSchema">
+            Create string field "{{newFieldName}}" for schema "{{lastSchema.name}}"
+        </button> <br>
+    </template>
 
 </template>
 
 
 
 <script setup lang="ts">
-    import { reactive } from 'vue';
+    import { reactive, ref } from 'vue';
 
     import { FieldType, Schema } from '@/models/schemas';
     import { SchemaStore } from '@/stores/schemaStore';
 
     const schemasStore = SchemaStore();
-    console.log(schemasStore.getSchemas());
-
     const schemas = schemasStore.getSchemas();
 
-    var lastSchema: any;
+    let newSchemaName = ref('');
+    let newFieldName = ref('');
 
+
+    var lastSchema: any = ref(undefined);
+    let schemaSet: any = ref(false);
     function addNewSchema() {
-        lastSchema = schemasStore.createSchema('topics');
+        lastSchema.value = schemasStore.createSchema(newSchemaName.value);
+        schemaSet.value = true;
+        newFieldName.value = '';
     }
 
     function updateLastSchema() {
-        lastSchema.addSimpleField('name', FieldType.string);
+        lastSchema.value.addSimpleField(newFieldName.value, FieldType.string);
     }
 
 </script>
