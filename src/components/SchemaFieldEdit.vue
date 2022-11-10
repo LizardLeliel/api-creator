c50b306ad5e57a611aedcc62a98a4f42e3233d11
LizardLeliel <shadow.shade.shine@gmail.com>
2022-11-10 12:23:59 -0400

    Stylized description edit textbox further

--- src/components/SchemaFieldEdit.vue
+++ src/components/SchemaFieldEdit.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="field-info">
-        <h5 class="w-100"> 
-            Field name: {{ field.label }} 
+        <h5 class="w-100 ps-1"> 
+            <u>Field</u>: <span>{{ field.label }} </span>
+            <!-- <u>Field</u>: <input class="write-field-name" type="text" v-model="editLabel" /> -->
+            <!-- &nbsp; &nbsp; <button type="button" class="btn btn-dark" @click="updateName"> Save Name </button> -->
 
             <span class="position-absolute end-0" style="width: 200px;">
                 &emsp; <input class="form-check-input" type="checkbox" v-model="editRequired"  /> &nbsp;
@@ -12,7 +14,12 @@
         </h5>
 
 
-        <div class="row">
+        <p class="description-preview" v-if="!isEditingDescription" @click="toggleEditingDescription()"> {{ field.description }} </p>
+        <div v-if="isEditingDescription">
+            <textarea ref="editDescBox" class="write-description-name" :v-model="editDescription">{{ editDescription }}</textarea>
+
+        </div>
+        <div class="row ps-1">
             <div class="col-12"> Type: {{field.type}} </div>
         </div>
     </div> 
@@ -35,18 +42,66 @@
     const schema = <Schema>inject('schema');
 
     let editRequired = ref(props.field.required);
-    let editLabel = ref(props.field.label);
+    let editDescription = ref(props.field.description);
+    // let editLabel = ref(props.field.label);
     let editType = ref(props.field.type);
+
+    let isEditingDescription = ref(false);
+
+    const editDescBox = ref<HTMLTextAreaElement | null>(null);
+
+    watch(editDescBox, (newValue, oldValue) => {
+        if (newValue != null) {
+            // TODO: Find out why "-1" is the magic number and how to make this more programmatic.
+            // (Maybe "scrollheight" accounts for border but height doesn't?)
+            (<HTMLTextAreaElement>newValue).style.height = (<HTMLTextAreaElement>newValue).scrollHeight + 1 + 'px';
+        }
+    })
 
     watch(editRequired, (newValue, oldValue) => {
         schemasStore.changeSchemaFieldRequired(schema.name, props.field.label, newValue);
     });
+
+    function toggleEditingDescription() {
+        isEditingDescription.value = !isEditingDescription.value;
+    }
 </script>
 
 
 
 
 <style scoped lang="scss">
+.description-preview {
+    width: 80%;
+    padding: .5rem;
+
+    cursor: pointer;
+    &:hover {
+        text-decoration: underline dotted;
+    }
+}
+
+.write-description-name {
+    border: 0px;
+
+    border-bottom: 1px solid lightgray;
+    width: 80%;
+
+    min-height: min-content;
+
+    padding: .5rem;
+    // padding: 0px;
+    margin-bottom: calc(16px - 1px); // Keep the same margin as <p> but account for the border
+    background-color: #f9f9f9;
+}
+
+.write-field-name {
+    border: 0px;
+
+    border-bottom: 1px solid lightgray;
+    width: 33%;
+}
+
 .field-info {
     padding-top: 1rem;
     padding-bottom: 1rem;
@@ -61,6 +116,4 @@
         font-size: .8rem;
     }
 }
-
-
 </style>
