c94d572b32cad35151a8736a01d4f6682d71311c
LizardLeliel <shadow.shade.shine@gmail.com>
2022-11-11 11:29:21 -0400

    modal disables background interaction while open

--- src/components/ModalWrapper.vue
+++ src/components/ModalWrapper.vue
@@ -1,7 +1,9 @@
 <template>
-    <div>
-        <slot></slot>
-    </div>
+    <div class="outer">
+        <div class="inner">
+            <slot></slot>
+        </div>
+    </div> 
 </template>
 
 
@@ -14,7 +16,19 @@
 
 
 <style scoped lang="scss">
-    div {
+    div.outer {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+
+        background-color: #6666;
+
+        z-index: 9998;
+    }
+
+    div.inner {
         position: fixed;
         width: 50%;
         margin-left: 25%;
